9b1535d9abf53941a76e28dfcac2c6ae706d1eb1
Santhosh <santhoshmahesh2001@gmail.com>
2022-07-23 12:16:33 +0530

    changes

--- quiz_app/views.py
+++ quiz_app/views.py
@@ -33,8 +33,7 @@ def register(request):
     })    
 @login_required
 def special(request):
-    # Remember to also set login url in settings.py!
-    # LOGIN_URL = '/basic_app/user_login/'
+  
     return HttpResponse("You are logged in. Nice!")
 
 @login_required
@@ -45,24 +44,18 @@ def user_logout(request):
 def user_login(request):
     
     if request.method == 'POST':
-        # First get the username and password supplied
+  
         username = request.POST.get('username')
         password = request.POST.get('password')
-
-        # Django's built-in authentication function:
         user = authenticate(username=username, password=password)
 
         # If we have a user
         if user:
-            #Check it the account is active
             if user.is_active:
-                # Log the user in.
                 login(request,user)
-                # Send the user back to some page.
-                # In this case their homepage.
                 return HttpResponseRedirect(reverse('index'))
             else:
-                # If account is not active:
+    
                 return HttpResponse("Your account is not active.")
         else:
             print("Someone tried to login and failed.")
@@ -70,7 +63,7 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
 
     else:
-        #Nothing has been provided for username or password.
+       
         return render(request, 'quiz_app/login.html', {})
 
 
